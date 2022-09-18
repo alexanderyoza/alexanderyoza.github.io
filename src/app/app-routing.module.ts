567749134c61e37d87ba408070219adfa72a6da8
alexanderyoza <alex.yoza@gmail.com>
2022-09-17 23:55:38 -0700

    Publish and routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { StaffComponent } from './staff/staff.component';
+import { PatientsComponent } from './patients/patients.component';
+import { OurOfficeComponent } from './our-office/our-office.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'staff', component: StaffComponent },
+  { path: 'patients', component: PatientsComponent },
+  { path: 'our-office', component: OurOfficeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StaffComponent } from './staff/staff.component';
+import { PatientsComponent } from './patients/patients.component';
+import { OurOfficeComponent } from './our-office/our-office.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaffComponent,
+    PatientsComponent,
+    OurOfficeComponent,
+    ServicesComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
